734a7ecd534812d697b64177f8cd5914775ee7df
rajendrakr85 <42311028+rajendrakr85@users.noreply.github.com>
2023-10-20 01:43:25 +0530

    Common Service commited

---
+++ CommonService/src/main/java/com/common/modal/Product.java
@@ -0,0 +1,13 @@
+package com.common.modal;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+    Integer productId;
+    String productName;
+}
